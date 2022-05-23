e0a9401057cd890b2ef3579ebd3cc8673bc815d2
zellytozelly <zellytozelly@gmail.com>
2022-05-23 19:05:31 +0900

    Feat: Set period data
    Co-authored-by: LUMPEN <lumpenop@users.noreply.github.com>

---
+++ src/routes/Dashboard/AllAdsStatus/Period/index.tsx
@@ -0,0 +1,100 @@
+import { useMemo } from 'react'
+import { BigNumber } from 'bignumber.js'
+
+import data from 'config/trend-data-set.json'
+import { IItem, IItemResult } from 'types/dashboard'
+
+import styles from './periodPerformance.module.scss'
+
+const newData = data.report.daily.map((item) => {
+  const bigNum: BigNumber = new BigNumber(item.roas).dividedBy(100).multipliedBy(item.cost)
+  const sales = Math.round(bigNum.toNumber() * 100) / 100
+  return { ...item, sales }
+})
+
+const PeriodPerformance = () => {
+  const thickFormat = ['ROAS', '광고비', '노출수', '클릭수', '전환수', '매출']
+
+  const plusItems = (arr: IItem[]) => {
+    const result = {
+      roas: 0,
+      cost: 0,
+      imp: 0,
+      click: 0,
+      conv: 0,
+      sales: 0,
+    }
+    arr.forEach((item: IItem) => {
+      result.roas += item.roas
+      result.cost += item.cost
+      result.imp += item.imp
+      result.click += item.click
+      result.conv += item.conv
+      result.sales += item.sales
+    })
+
+    return result
+  }
+
+  const minusItem = (current: IItemResult, past: IItemResult) => {
+    const result: number[] = []
+    result.push(current.roas - past.roas)
+    // result.push(((current.sales + past.sales) / (current.cost + past.cost)) * 100)
+    result.push(current.cost - past.cost)
+    result.push(current.imp - past.imp)
+    result.push(current.click - past.click)
+    result.push(current.conv - past.conv)
+    result.push(current.sales - past.sales)
+
+    return result
+  }
+
+  const usePeriodItems = (days: number) => {
+    const currentArray = newData.slice(-days)
+    const pastArray = newData.slice(-days * 2, -days)
+
+    const calculatedCurrent = plusItems(currentArray)
+    const calculatedPast = plusItems(pastArray)
+
+    const periodItem = minusItem(calculatedCurrent, calculatedPast)
+    const result = { calculatedCurrent, periodItem }
+
+    return result
+  }
+  const day = 3
+  const { calculatedCurrent, periodItem } = usePeriodItems(day)
+
+  const getValues = (obj: object) => {
+    const result = []
+    for (const [key, value] of Object.entries(obj)) {
+      result.push(value)
+    }
+    return result
+  }
+  const currentItems = getValues(calculatedCurrent)
+
+  const periodItems = useMemo(() => {
+    return (
+      <ul>
+        {periodItem.map((item: number, index: number) => {
+          return (
+            <li key={thickFormat[index]} className={styles.container}>
+              <div>
+                <span>{thickFormat[index]}</span>
+                <p>{currentItems[index]}%</p>
+              </div>
+              <div>
+                <div>▼</div>
+                <span>{item}</span>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }, [data])
+
+  return <section>{periodItems}</section>
+}
+
+export default PeriodPerformance
--- src/routes/Dashboard/AllAdsStatus/index.tsx
+++ src/routes/Dashboard/AllAdsStatus/index.tsx
@@ -1,43 +1,14 @@
-import { useMemo } from 'react'
-
+import Period from './Period'
 import AllStatusChart from './AllStatusChart'
-import PeriodPerformance from './Period/PeriodPerformance'
-import data from '../../../config/trend-data-set.json'
 
-import { BigNumber } from 'bignumber.js'
 import styles from './allAdsStatus.module.scss'
 
-BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_HALF_UP })
-
-const newData = data.report.daily.map((item, index) => {
-  const bigNum: BigNumber = new BigNumber(item.roas).dividedBy(100).multipliedBy(item.cost)
-  const sales = Math.round(bigNum.toNumber() * 100) / 100
-  return { ...item, sales }
-})
-
-console.log('매출=', (data.report.daily[0].roas / 100) * data.report.daily[0].cost)
-console.log(newData[0])
-
 const Integrated = () => {
-  const thickFormat = ['ROAS', '광고비', '매출', '노출수', '클릭수', '전환수']
-
-  const usePeriodItems = (dailyKey: String) => {
-    useMemo(() => {
-      return (
-        <ul>
-          {data.report.daily.map((item, index) => {
-            return <li key={item.date}>period</li>
-          })}
-        </ul>
-      )
-    }, [data])
-  }
-
   return (
     <div className={styles.container}>
       <h1>통합 광고 현황</h1>
       <div className={styles.contents}>
-        <PeriodPerformance />
+        <Period />
         <AllStatusChart />
       </div>
     </div>
