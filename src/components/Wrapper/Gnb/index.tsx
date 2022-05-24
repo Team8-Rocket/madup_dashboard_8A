3df5d76cc9d5e1edf292e660ef57aafaa3b880ce
권은서 <kwonenseo@gmail.com>
2022-05-24 16:10:25 +0900

    feat: Create GNB
    TODO
    - 광고센터 아이콘 색 클릭 시 바꾸기

--- src/components/Wrapper/Gnb/index.tsx
+++ src/components/Wrapper/Gnb/index.tsx
@@ -1,23 +1,53 @@
+import cx from 'classnames'
 import styles from './gnb.module.scss'
 import { NavLink, Outlet } from 'react-router-dom'
+import { LeverBi, DashboardIcon, AdvertiseIcon, GuideIcon } from '../../../assets/svgs'
+import Dropdown from '../../Dropdown'
 
 const Gnb = () => {
   return (
-    <div>
-      <div className={styles.gnbWrapper}>
-        <header>head</header>
-        <nav>
+    <div className={styles.gnbWrapper}>
+      <header>
+        <LeverBi />
+      </header>
+      <nav>
+        <div className={styles.service}>
+          <h4>서비스</h4>
+          <Dropdown />
+        </div>
+        <div className={styles.linkList}>
+          <h4>광고 센터</h4>
           <ul>
             <li>
-              <NavLink to=''>대시보드</NavLink>
+              <NavLink to='' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+                <DashboardIcon />
+                <h5>대시보드</h5>
+              </NavLink>
             </li>
             <li>
-              <NavLink to='advertise'>광고관리</NavLink>
+              <NavLink to='advertise' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+                <AdvertiseIcon />
+                <h5>광고관리</h5>
+              </NavLink>
             </li>
           </ul>
-        </nav>
-        <footer>이용 가이드</footer>
-      </div>
+        </div>
+      </nav>
+      <footer>
+        <div className={styles.guide}>
+          <div className={styles.guideIcon}>
+            <GuideIcon />
+          </div>
+          <div className={styles.guideContent}>
+            <p>레버 이용 가이드</p>
+            <span>시작하기 전에 알아보기</span>
+          </div>
+        </div>
+        <div className={styles.agreement}>
+          <p>레버는 함께 만들어갑니다.</p>
+          <span>이용약관</span>
+        </div>
+      </footer>
     </div>
   )
 }
