f271f02f7afaea8a7156c68d8853e4cc2333d0f5
bisari <bisari31@gmail.com>
2022-05-24 10:15:53 +0900

    Feat: Add Advertise page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+
+import './styles/index.scss'
+
 import Routes from './routes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import styles from './dashboard.module.scss'
 
 const Dashboard = () => {
   return (
-    <main className={styles.appWrapper}>
+    <section className={styles.appWrapper}>
       <header>
         <h1>대시보드 or 광고관리</h1>
         <p>날짜 or x</p>
@@ -11,7 +11,7 @@ const Dashboard = () => {
         <div>통합 광고 현황 컴포넌트</div>
         <div>매체 현황 컴포넌트</div>
       </section>
-    </main>
+    </section>
   )
 }
 
